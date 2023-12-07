cb73f39335af21c5fce6c0f2f7e0e3b4335f089e
Nimo08 <nimomohamed800@gmail.com>
2023-12-07 20:57:52 +0300

    Tasks 1,2,3

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,43 @@
+// Script must be executed only when DOM is loaded
+// Listen for changes on each input checkbox tag:
+// If the checkbox is checked, you must store the Amenity ID
+// in a variable (dictionary or list)
+// If the checkbox is unchecked, you must remove the Amenity ID
+// from the variable
+// Update the h4 tag inside the div Amenities with the list
+// of Amenities checked
+
+$(document).ready(function () {
+  const isChecked = {};
+
+  // listen for changes
+  $('input[type="checkbox"]').change(function () {
+    const amenityID = $(this).data('id');
+    const amenityName = $(this).data('name');
+
+    if ($(this).is(':checked')) {
+      isChecked[amenityID] = amenityName;
+    } else {
+      delete isChecked[amenityID];
+    }
+    updateAmenities();
+  });
+
+  function updateAmenities () {
+    // object.values: extract array of all value
+    const listAmenities = Object.values(isChecked).join(', ');
+    $('.amenities h4').text('Amenities: ' + listAmenities);
+  }
+  $.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/status/',
+    type: 'GET',
+    dataType: 'json',
+    success: function (data) {
+      if (data.status === 'OK') {
+        $('#api_status').addClass('available');
+      } else {
+        $('#api_status').removeClass('available');
+      }
+    }
+  });
+});
